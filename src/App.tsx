1452efd2641384c5c1c45e1588d6e40c51882b54
Nipan Debnath <debnathnipan719@gmail.com>
2023-10-23 13:24:55 +0530

    update route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./layout";
+import Home from "./pages/Home";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+      ],
       errorElement: (
         <div className="p-5 text-red-800 bg-red-50">'404 Not Found'</div>
       ),
