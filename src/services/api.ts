18944a626c300ad86aebfa5a116130db635b2147
Nipan Debnath <debnathnipan719@gmail.com>
2023-10-25 00:59:12 +0530

    update for car's image api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,9 @@
-export const fetchCars = async (query: string) => {
-  const BASE_URL =
-    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla" + query;
+import { CarProps, FetchOptions } from "../types";
+
+export const fetchCars = async (options: FetchOptions) => {
+  const BASE_URL = "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?";
+
+  const { manufacturer, model, year, limit, fuel } = options;
 
   const headers = {
     "X-RapidAPI-Key": import.meta.env.VITE_X_RAPIDAPI_KEY,
@@ -8,10 +11,13 @@ export const fetchCars = async (query: string) => {
   };
 
   try {
-    const res = await fetch(BASE_URL, {
-      method: "GET",
-      headers,
-    });
+    const res = await fetch(
+      `${BASE_URL}make=${manufacturer}&model=${model}&year=${year}&limit=${limit}&fuel_type=${fuel}`,
+      {
+        method: "GET",
+        headers,
+      }
+    );
     const result = res.json();
     return result;
   } catch (error) {
@@ -19,4 +25,19 @@ export const fetchCars = async (query: string) => {
 
     return error;
   }
+};
+
+export const getCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+
+  const { make, model, year } = car;
+
+  url.searchParams.append("customer", "hrjavascript-mastery");
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  url.searchParams.append("angle", `${angle ?? ""}`);
+
+  return `${url}`;
 };
