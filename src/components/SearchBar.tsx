abb8899df2ad92000cc31e537d6d47f2dc9004b7
Nipan Debnath <debnathnipan719@gmail.com>
2023-10-24 23:59:24 +0530

    update searchbar by filtering

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,15 +1,66 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
 import SearchManufacturer from "./SearchManufacturer";
+import { useSearchParams } from "react-router-dom";
+import { updateSearchParams } from "../utils";
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState("");
+  const [model, setModel] = useState("");
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    updateSearchParams(searchParams, "manufacturer", manufacturer);
+    updateSearchParams(searchParams, "model", model);
+    setSearchParams(searchParams, { replace: true });
+  };
 
   return (
-    <form>
+    <form className="searchbar relative" onSubmit={handleSubmit}>
       <SearchManufacturer
         manufacturer={manufacturer}
         setManufacturer={setManufacturer}
       ></SearchManufacturer>
+      <button
+        type="submit"
+        className="absolute right-3 top-2.5 w-7 h-7 sm:hidden"
+      >
+        <img
+          src="/magnifying-glass.svg"
+          alt="search icon"
+          width={24}
+          height={24}
+          className="w-7 h-7"
+        />
+      </button>
+
+      <div className="searchbar__item relative">
+        <img
+          src="/model-icon.png"
+          alt="car model"
+          width={25}
+          height={25}
+          className="absolute w-5 h-5 ml-4"
+        />
+        <input
+          type="text"
+          name="model"
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder="Tiguan"
+          className="searchbar__input"
+        />
+        <button type="submit" className="absolute right-3 top-2.5 w-7 h-7">
+          <img
+            src="/magnifying-glass.svg"
+            alt="search icon"
+            width={24}
+            height={24}
+            className="w-7 h-7"
+          />
+        </button>
+      </div>
     </form>
   );
 };
