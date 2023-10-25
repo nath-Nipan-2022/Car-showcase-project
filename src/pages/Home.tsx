396efa5622d9d3724264ff50cd6f4db4350d67e4
Nipan Debnath <debnathnipan719@gmail.com>
2023-10-25 22:10:06 +0530

    ✨ feat: loader & showMore

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,33 +4,45 @@ import { CarProps } from "../types";
 import Filter from "../components/Filter";
 import Hero from "../components/Hero";
 import SearchBar from "../components/SearchBar";
+import CarItem from "../components/CarItem";
+import ShowMore from "../components/ShowMore";
 
 import { fetchCars } from "../services/api";
-import CarItem from "../components/CarItem";
 import { useSearchParams } from "react-router-dom";
 import { fuels, yearsOfProduction } from "../constants";
+import { updateSearchParams } from "../utils";
 
 const Home = () => {
   const [cars, setCars] = useState<CarProps[]>([]);
-  const [searchParams] = useSearchParams();
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const limit = Number(searchParams.get("limit") ?? 10);
+
+  const handleNavigation = () => {
+    const nextLimit = limit + 1 * 10;
+    updateSearchParams(searchParams, "limit", `${nextLimit}`);
+    setSearchParams(searchParams, { replace: true });
+  };
 
   useEffect(() => {
+    setIsLoading(true);
     fetchCars({
       manufacturer: searchParams.get("manufacturer") ?? "",
       model: searchParams.get("model") ?? "",
-      year: Number(searchParams.get("year")) ?? 2022,
+      year: Number(searchParams.get("year") ?? 2022),
       fuel: searchParams.get("fuel") ?? "",
-      limit: Number(searchParams.get("limit")) ?? 10,
+      limit,
     }).then((cars) => {
       setCars(cars);
+      setIsLoading(false);
     });
-  }, [searchParams]);
+  }, [searchParams, limit]);
 
   return (
     <>
       <Hero />
 
-      <section className="mt-12 padding-x padding-y max-width" id="discover">
+      <section className="mt-12 padding-x padding-y max-width" id="explore">
         <h2 className="text-4xl font-extrabold text-black-100">
           Car Catalogue
         </h2>
@@ -48,12 +60,27 @@ const Home = () => {
 
       <section>
         <div className="home__cars-wrapper padding-x ">
-          {cars.length > 0 ? (
-            cars.map((car, i) => <CarItem key={i} car={car} />)
-          ) : (
-            <div className="text-red-900 bg-red-200">NO DATA FOUND</div>
-          )}
+          {cars.length > 0
+            ? cars.map((car, i) => <CarItem key={i} car={car} />)
+            : !isLoading && (
+                <div className="home__error-container">
+                  <h3 className="text-black-100 text-xl font-bold">
+                    Oops, No Data Found
+                  </h3>
+                </div>
+              )}
         </div>
+
+        {/* Loader */}
+        {isLoading && (
+          <div className="grid h-16 mt-10 place-items-center">
+            <div>
+              <span className="w-10 h-10 border-[5px] border-primary-blue border-b-transparent rounded-full inline-block animate-spin"></span>
+            </div>
+          </div>
+        )}
+
+        <ShowMore hasMore={cars.length >= limit} onClick={handleNavigation} />
       </section>
     </>
   );
